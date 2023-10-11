9ae172e650603d7c2187f16f969387e03e0542ea
dator <dennizyilmaz1903@gmail.com>
2023-10-11 12:54:37 +0200

    first commit

---
+++ QuizUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuizUp
+//
+//  Created by dator on 2023-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to QuizUp!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuizUp/QuizUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizUpApp.swift
+//  QuizUp
+//
+//  Created by dator on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct QuizUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
