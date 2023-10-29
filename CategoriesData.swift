6eebacd35df9ab47bfa710c0b920438bfdd42460
dator <dennizyilmaz1903@gmail.com>
2023-10-29 13:48:26 +0100

    fixed leaderboard and other stuff

--- CategoriesData.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import SwiftUI
-
-// To store information of all the categories
-struct CategoriesData: Identifiable {
-        var id: UUID = UUID() // Each category have an unique ID
-        var categorieName: String
-}
